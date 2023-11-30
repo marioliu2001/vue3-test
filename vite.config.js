ebd1954b9e20b1926eed8c5daebbac5723a1bb34
LXL <1102549801@qq.com>
2023-11-30 14:53:17 +0800

    fix:修复路径问题

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue3-template-admin/',
   // env文件目录
   envDir: 'env',
   // 环境变量前缀
