d386e7f30ee8380c919276863aefdf9ec733d57a
marioliu2001 <62660112+marioliu2001@users.noreply.github.com>
2023-11-30 07:31:48 +0000

    Deploying to gh-pages from @ marioliu2001/vue3-test@fedff0f0738fcd72bb32c5f0db3ba92ad15070a9 🚀

--- assets/index-32b7a290.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as s}from"./index-c359d0f1.js";const c={},n={class:"settings"};function o(r,_){return t(),s("div",n," Settings ")}const i=e(c,[["render",o]]);export{i as default};
--- assets/useInstance-37baf641.js
+++
@@ -1,1 +0,0 @@
-import{ai as e}from"./index-c359d0f1.js";function a(){const{appContext:t,proxy:n}=e(),o=t.config.globalProperties;return{proxy:n,global:o}}export{a as u};
